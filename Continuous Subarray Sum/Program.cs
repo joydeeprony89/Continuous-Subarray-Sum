c53376dfb4543b037ab2fdb30301f9e6f80f9857
jd_786 <jaydeep_shil@certree.com>
2022-06-24 19:08:25 +0530

    added comments

--- Continuous Subarray Sum/Program.cs
+++ Continuous Subarray Sum/Program.cs
@@ -26,9 +26,14 @@ namespace Continuous_Subarray_Sum
       for (int i = 0; i < nums.Length; i++)
       {
         sum += nums[i];
+        // example say sum is 23 and k is 6, 23 % 6 = 1, 1 say we need 1 to be present so that 23+1 = 24 is the multiple of 6
         int mod = sum % k;
         if (hash.ContainsKey(mod))
         {
+          // so 23 we have found and we need 1 to make this multiple of 6
+          // if we found 1 as well, then check the 1 found index is lesser than i(current index), because we want continuous subarray
+          // 1 has to appear before 23 => (1, 23) and also we need atleast 2 element whose sum is multiple of k
+          // thats why we check i - currentIndex > 1
           int previousIndex = hash[mod];
           if (i - previousIndex > 1) return true;
         }
